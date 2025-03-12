8c274d2b8e0044ff395e279145135ceaaa122066
kuldip.phadatare <kuldip.phadatare1@allianz.com>
2025-03-12 23:52:05 +0530

    Updated latest changes

---
+++ src/main/java/com/actions/demo/GitHuabActionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.actions.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHuabActionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHuabActionDemoApplication.class, args);
+	}
+
+}
